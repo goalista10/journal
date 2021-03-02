9a7b2f83b78f275a807304642f9c583b4fa68f39
Angelo <goalista@gmail.com>
2021-03-02 19:15:59 +0800

    add CRUD for tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,38 @@
+class TasksController < ApplicationController
+    def index
+        @show_tasks = current_user.categories.find(params[:category_id]).tasks
+        @category = current_user.categories.find(params[:category_id])
+        @build_task = current_user.categories.find(params[:category_id]).tasks.build
+        render "task/index"
+    end
+
+    def create
+        @create_task = current_user.categories.find(params[:category_id]).tasks.build(task_params)
+        @create_task.save
+        flash.notice = "Task Successfully created"
+        redirect_to category_tasks_path(params[:category_id])
+    end
+
+    def edit
+        @to_be_updated = current_user.categories.find(params[:category_id]).tasks.find(params[:id])
+        render "task/edit"
+    end
+
+    def update
+        current_user.categories.find(params[:category_id]).tasks.find(params[:id]).update(task_params)
+        flash.notice = "Category Successfully updated"
+        redirect_to category_tasks_path
+    end
+
+    def destroy
+        @to_be_deleted = current_user.categories.find(params[:category_id]).tasks.find(params[:id])
+        @to_be_deleted.destroy
+        redirect_to category_tasks_path
+    end
+
+    private
+    # Only allow a list of trusted parameters through.
+    def task_params
+      params.require(:task).permit(:name,:category_id)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users , controllers: {
     sessions: 'users/sessions'
   }
-  resources :categories
+  resources :categories do
+    resources :tasks
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "home/homepage"
   root "home#homepage"
