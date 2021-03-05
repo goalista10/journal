cc2315cb73014eaebc1b355f68c9bc9ec3214510
Angelo <goalista@gmail.com>
2021-03-05 16:22:02 +0800

    bootstrapified

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,5 +118,19 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { :host => 'yourhostsite.herokuapp.com' }
+  #config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  ActionMailer::Base.smtp_settings =
+    {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :authentication => :plain,
+      :domain => "gmail.com",
+      :user_name => "for.testing.ange@gmail.com",
+      :password => "testP@$$w0rd10!",
+    }
 end
