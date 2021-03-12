da7ac20e30c345acf652ea62cbef1161a52de8f2
Angelo <goalista@gmail.com>
2021-03-12 23:06:18 +0800

    added tests for category

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -10,22 +10,44 @@ class BlogControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "create category " do
+  setup do
+    get '/users/sign_in'
     sign_in users(:user_001)
+    post user_session_url
+  end
 
-    assert_difference 'current_user.categories.count' do
+  test "create category " do
+    @first_user = User.first
+    assert_difference '@first_user.categories.count' do
       post categories_path , params: { category: { name: "Test" } }
     end
   end
 
   test "go to edit category " do
-    sign_in users(:user_001)
-
-    new_category = current_user.categories.build(name: "test", user_id: current_user.id)
+    @first_user = User.first
+    new_category = @first_user.categories.build(name: "test")
     new_category.save
-
-    get edit_category_path(new_category.id)
+    
+    get edit_category_path(new_category)
     assert_response :success
+  end
+
+  test "edit category " do
+    @first_user = User.first
+    new_category = @first_user.categories.build(name: "test")
+    new_category.save
+    
+    patch category_path(new_category) , params: { category: { name: "Test2" } }
+    assert_response :redirect
+  end
+
+  test "delete category " do
+    @first_user = User.first
+    new_category = @first_user.categories.build(name: "test")
+    new_category.save
+    
+    delete category_path(new_category)
+    assert_response :redirect
   end
 
 end
