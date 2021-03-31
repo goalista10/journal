54b885dc187c059695efcb7418d02a15f80a45aa
Angelo <goalista@gmail.com>
2021-03-31 20:25:41 +0800

    add case insensitivity for category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-    validates :name, presence: true, uniqueness: { scope: :user_id }
+    validates :name, presence: true, uniqueness: { scope: :user_id ,:case_sensitive => false}
     belongs_to :user
     has_many :tasks , dependent: :destroy
 end
